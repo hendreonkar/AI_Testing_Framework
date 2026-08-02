176d6665ca851bde534e4c42f38c78a7531a2f17
Onkar Hendre <hendreonkar@gmail.com>
2026-08-02 13:46:33 +0530

    Addition_default_num.py is added

---
+++ Python_learning/Practice_programs/Addition_default_num.py
@@ -0,0 +1,9 @@
+n1=input("Enter value n1: ")
+n2=input("Enter value n2: ")
+n3=input("Enter value n3: ")
+
+n1=int(n1) if n1 else 100
+n2=int(n2) if n2 else 200
+n3=int(n3) if n3 else 300
+
+print(n1+n2+n3)
